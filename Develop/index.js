68ea092f6eb6058923afb0812725be6eb0bb8c39
Yasmin Bustami <yasminbustami@Yasmins-MacBook-Pro.local>
2022-09-25 01:39:06 -0500

    complete Index

--- Develop/index.js
+++ Develop/index.js
@@ -55,7 +55,12 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (data) {
+        writeToFile("./utils/README.md", generateMarkdown(data));
+    })
+}
 
 // Function call to initialize app
 init();
